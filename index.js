1b3e79bae179cb9064d7c622292f320331132b93
alfredofloresnt <alfredofloresn.t@gmail.com>
2020-11-12 16:14:42 -0600

    Added session

--- index.js
+++ index.js
@@ -5,11 +5,18 @@ var port = process.env.PORT || 4000;
 
 const app = express();
 
+var allowCrossDomain = function(req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    next();
+};
+
 // Connectar a BD
 const connection = mysql.createConnection({
     host: "localhost",
     user: "root",
-    password: "Root!@#$"
+    password: "root"
   });
 
 connection.connect(function(err) {
@@ -19,6 +26,7 @@ connection.connect(function(err) {
 
 
 // Iniciar Servidor
+app.use(allowCrossDomain);
 app.use(bodyParser.json())
 app.use('/', require('./Routes/routes'));
 
