75d4009c14cdda014385887974a64e3cc525bd2b
Andres Orozco <andresorocast.aoc@gmail.com>
2020-11-24 00:45:11 -0600

    corregir query de checkHospital

--- Models/Hospital.js
+++ Models/Hospital.js
@@ -16,10 +16,11 @@ exports.getHospital = (hospital, callback) => {
 }
 
 exports.checkHospital = (hospital, callback) => {
-  let sql = "SELECT h.idHospital, count(h.name) as total FROM hospital h WHERE h.name = " + connection.escape(hospital)
+  let sql = "SELECT h.idHospital, count(h.name) as total FROM hospital h WHERE h.name = " + connection.escape(hospital) + " GROUP BY h.idHospital"
   connection.query(sql, function (err, result) {
     if (err) throw err;
-    if (result[0].total > 0) {
+    console.log(result[0]);
+    if (result[0] != undefined) {
       callback(result[0].idHospital);
     } else {
       sql = "INSERT INTO hospital (name) VALUES (" + connection.escape(hospital) + ")";
